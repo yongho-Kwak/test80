cd3c4d6f1b0dd959819666e48a85a777808442e3
yongho-Kwak <indrakid@hotmail.com>
2016-04-20 15:42:21 +0900

    '.'

--- test01/src/Hello2.java
+++ test01/src/Hello2.java
@@ -4,6 +4,8 @@ public class Hello2 {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     System.out.println("Hello");
+    System.out.println("okok");
+    System.out.println("nono");
   }
 
 }
